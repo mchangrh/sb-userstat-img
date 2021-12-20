c827071e72552898297f627715f980a40a172c33
Michael C <michael@mchang.name>
2021-12-20 15:23:00 -0500

    double size and reduce png compression

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -4,17 +4,21 @@ const axios = require("axios")
 const Canvas = require("canvas")
 const BASEURL = "https://sponsor.ajay.app/api"
 
+const PNGConfig = {
+  compressionLevel: 3
+}
+
 const categories = async (request, reply) => {
   const userID = request.query.userID;
   const res = await axios.get(`${BASEURL}/userStats?publicUserID=${userID}&fetchCategoryStats=true`)
   const categoryData = Object.values(res.data.categoryCount)
   const total = categoryData.reduce((a, b) => a + b, 0)
-  const canvas = Canvas.createCanvas(325, 200);
+  const canvas = Canvas.createCanvas(650, 400);
   const ctx = canvas.getContext("2d");
   const x = canvas.width / 3.25;
   const y = canvas.height / 2;
   let startAngle = -Math.PI / 2;
-  ctx.font = "13px DejaVu Sans";
+  ctx.font = "20px DejaVu Sans";
   for (let i = 0; i < categoryData.length; i++) {
     // create slice of pie chart
     const sliceAngle = 2 * Math.PI * categoryData[i] / total;
@@ -28,12 +32,12 @@ const categories = async (request, reply) => {
     // create square label
     const rectX = canvas.width - x * 1.25 + 5;
     const rectY = canvas.height / (categoryData.length + 1) * (i + 1) - 7.5;
-    const rectSize = 15;
+    const rectSize = 30;
     ctx.fillRect(rectX, rectY, rectSize, rectSize);
     // create label text
-    ctx.fillText(CATEGORIES_ARR[i], rectX + 20, rectY + rectSize * 0.75);
+    ctx.fillText(CATEGORIES_ARR[i], rectX + 35, rectY + rectSize * 0.75);
   }
-  reply.type("image/png").send(canvas.createPNGStream())
+  reply.type("image/png").send(canvas.createPNGStream(PNGConfig))
 }
 
 module.exports = {
